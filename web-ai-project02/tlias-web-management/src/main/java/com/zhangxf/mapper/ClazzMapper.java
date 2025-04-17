a7beb46f4b876afa14c00e2d231f959281e1e18d
lsweet52 <3557558991@qq.com>
2025-04-17 15:40:35 +0800

    Update ClazzMapper.java

--- web-ai-project02/tlias-web-management/src/main/java/com/zhangxf/mapper/ClazzMapper.java
+++ web-ai-project02/tlias-web-management/src/main/java/com/zhangxf/mapper/ClazzMapper.java
@@ -2,7 +2,9 @@ package com.zhangxf.mapper;
 
 import com.zhangxf.pojo.Clazz;
 import com.zhangxf.pojo.ClazzQueryParam;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -13,4 +15,21 @@ public interface ClazzMapper {
      * 查询所有班级
      */
     List<Clazz> getAllClazz(ClazzQueryParam clazzQueryParam);
+
+    /**
+     * 新增班级
+     */
+    @Insert("insert into clazz values(null, #{name}, #{room}, #{beginDate}, #{endDate}, #{masterId}, #{subject}, #{createTime}, #{updateTime})")
+    void addClazz(Clazz clazz);
+
+    /**
+     * 根据id查询班级
+     */
+    @Select("select * from clazz where id = #{id}")
+    Clazz findClazzById(Integer id);
+
+    /**
+     * 修改班级
+     */
+    void updateClazz(Clazz clazz);
 }
