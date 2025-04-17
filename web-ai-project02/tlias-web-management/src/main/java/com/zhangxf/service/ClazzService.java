84f946c8c2a0e57830142a47d57d5013d8960052
lsweet52 <3557558991@qq.com>
2025-04-17 15:39:38 +0800

    Update ClazzService.java

--- web-ai-project02/tlias-web-management/src/main/java/com/zhangxf/service/ClazzService.java
+++ web-ai-project02/tlias-web-management/src/main/java/com/zhangxf/service/ClazzService.java
@@ -13,4 +13,19 @@ public interface ClazzService {
      * 查询所有班级
      */
     PageResult<Clazz> getAllClazz(ClazzQueryParam clazzQueryParam);
+
+    /**
+     * 新增班级
+     */
+    void addClazz(Clazz clazz);
+
+    /**
+     * 根据id查询班级
+     */
+    Clazz findClazzById(Integer id);
+
+    /**
+     * 修改班级
+     */
+    void updateClazz(Clazz clazz);
 }
