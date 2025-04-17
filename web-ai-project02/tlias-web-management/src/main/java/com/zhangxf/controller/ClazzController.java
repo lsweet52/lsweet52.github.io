7d93a85532dac40bfcea1afffd8760660b4923e2
lsweet52 <3557558991@qq.com>
2025-04-17 15:39:10 +0800

    Update ClazzController.java

--- web-ai-project02/tlias-web-management/src/main/java/com/zhangxf/controller/ClazzController.java
+++ web-ai-project02/tlias-web-management/src/main/java/com/zhangxf/controller/ClazzController.java
@@ -1,6 +1,5 @@
 package com.zhangxf.controller;
 
-import com.github.pagehelper.PageHelper;
 import com.zhangxf.pojo.Clazz;
 import com.zhangxf.pojo.ClazzQueryParam;
 import com.zhangxf.pojo.PageResult;
@@ -8,11 +7,8 @@ import com.zhangxf.pojo.Result;
 import com.zhangxf.service.ClazzService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Slf4j
 @RequestMapping("/clazzs")
@@ -22,6 +18,9 @@ public class ClazzController {
     @Autowired
     private ClazzService clazzService;
 
+    /**
+     * 分页查询所有班级
+     */
     @GetMapping
     public Result getClazz(ClazzQueryParam clazzQueryParam) {
         log.info("查询请求参数: {}", clazzQueryParam);
@@ -29,6 +28,34 @@ public class ClazzController {
         return Result.success(pageResult);
     }
 
-    
+    /**
+     * 新增班级
+     */
+    @PostMapping
+    public Result addClazz(@RequestBody Clazz clazz){
+        log.info("新增班级: {}", clazz);
+        clazzService.addClazz(clazz);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询班级
+     */
+    @GetMapping("/{id}")
+    public Result findClazzById(@PathVariable Integer id){
+        log.info("根据id查询班级: {}", id);
+        Clazz clazz = clazzService.findClazzById(id);
+        return Result.success(clazz);
+    }
+
+    /**
+     * 修改班级
+     */
+    @PutMapping
+    public Result updateClazz(@RequestBody Clazz clazz){
+        log.info("修改班级: {}", clazz);
+        clazzService.updateClazz(clazz);
+        return Result.success();
+    }
 
 }
